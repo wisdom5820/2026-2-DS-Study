0a181cf96c866e9ae1cd64e42c7c6f3f8ceb104c
eunhan91 <joonhyuk0901@gmail.com>
2026-09-14 05:04:07 +0900

    eunhan91 jungol # 8928

---
+++ jungol/eunhan91/sep_9th/8928.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T, k, N;
+    string S;
+    cin>>T>>k;
+
+    while(T--){
+        cin>>N>>S;
+
+        cout<<"YES"<<'\n';
+
+        if(k==0) continue;
+
+        for(int i=0; i<N-1; i++){
+            if(S[i]==S[i+1]) cout<<'M';
+
+            else cout<<'O';
+        }
+
+        cout<<'\n';
+    }
+
+}
