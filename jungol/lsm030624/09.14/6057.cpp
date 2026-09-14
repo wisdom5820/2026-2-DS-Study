0e2f4e53e570b04d682effef231ba87442b31be3
lsm030624 <126699042+lsm030624@users.noreply.github.com>
2026-09-14 17:17:48 +0900

    Update 6057.cpp

--- jungol/lsm030624/09.14/6057.cpp
+++ jungol/lsm030624/09.14/6057.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); // ios_base::sync_with_stdio(false);
+    cin.tie(0); // cin.tie(NULL);
+    int P, N;
+    cin >> P >> N;
+    vector<queue<pair<int, int>>> que(P);
+
+    int sum = 0;
+
+    for (int i = 0; i< N; i++){
+        int cmd, p, m;
+        cin >> cmd;
+        if (cmd == 0) cin >> p >> m;
+        else cin >> p;
+        switch(cmd){
+            case 0:
+                que[p-1].push({p, m});
+                break;
+            case 1:
+                if (que[p-1].size() > 0){
+                    sum = sum + que[p-1].front().second;
+                    que[p-1].pop();
+                }
+                break;
+        }
+    }
+    cout << sum;
+}
 
