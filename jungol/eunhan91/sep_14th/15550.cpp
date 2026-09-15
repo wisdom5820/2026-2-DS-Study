16741ea3761e80a63d68d04b53980b8bcec98f94
eunhan91 <joonhyuk0901@gmail.com>
2026-09-16 01:20:46 +0900

    eunhan91 jungol #15550 -v2 remove unused variable

--- jungol/eunhan91/sep_14th/15550.cpp
+++ jungol/eunhan91/sep_14th/15550.cpp
@@ -6,7 +6,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n,m,T,i,j,k;
+    int n,m,T,i,j;
     long long time=0;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> printer;//priority queue는 같은 값의 자료들을 구분하는 방법이 존재하지 않는다.
 
