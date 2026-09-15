cf6f25f27f578bbcffba553a79b06a8503caccd0
eunhan91 <joonhyuk0901@gmail.com>
2026-09-16 02:27:34 +0900

    eunhan91 jungol #15550 -v3 fix logical error

--- jungol/eunhan91/sep_14th/15550.cpp
+++ jungol/eunhan91/sep_14th/15550.cpp
@@ -6,27 +6,41 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n,m,T,i,j;
-    long long time=0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> printer;//priority queue는 같은 값의 자료들을 구분하는 방법이 존재하지 않는다.
+    int n,m,T,i,j, time;
 
     cin>>T;
     while(T--){
+        queue<pair<int, int>> printer;//priority queue는 같은 값의 자료들을 구분하는 방법이 존재하지 않는다.
+        priority_queue<int> priorities;
+
+        time=0;
+
         cin>>n>>m;
 
         for(i=0; i<n; i++){
             cin>>j; 
             
             printer.push({j, i});
+            priorities.push(j);
         }
 
-        while(true){
-            if(printer.top().second==m) break;
+        while(!printer.empty()){
+            auto [priority, index]=printer.front();
 
             printer.pop();
-            time+=1;
+
+            if(priority<priorities.top()){
+                printer.push({priority, index});
+                continue;
+            }
+
+            priorities.pop();
+            ++time;
+
+            if(index==m){
+                cout<<time<<'\n';
+                break;
+            }
         }
-        
-        cout<<time<<'\n';
     }
 }
