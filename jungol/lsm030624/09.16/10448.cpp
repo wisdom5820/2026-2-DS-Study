7c8557279a52602dc2835162cae84deb8cbb7ec1
lsm030624 <126699042+lsm030624@users.noreply.github.com>
2026-09-18 23:51:06 +0900

    Update 10448.cpp

--- jungol/lsm030624/09.16/10448.cpp
+++ jungol/lsm030624/09.16/10448.cpp
@@ -1,1 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); // ios::sync_with_stdio(false);
+	cin.tie(0); // cin.tie(nullptr);
+    int T;
+    cin >> T;
+    for (int j = 1; j<=T; j++){
+        map<char, int> m;
+        int i = 0;
+        for (; i<26; i++){
+            int val;
+            cin >> val;
+            m['A' + i] = val;
+        }
+        cin >> i;
+        set<string> str;
+
+        int sz = i;
+        while (i--){
+            string temp = "";
+            string s;
+            cin >> s;
+            for (const char& c : s){
+                temp = temp + to_string(m[c]);
+                //cout << "temp = "<< temp << endl;
+            }
+            str.insert(temp);
+        }
+        if (str.size() != sz){
+            cout << "Case #"<< j <<": YES\n";
+        }
+        else{
+            cout << "Case #"<< j <<": NO\n";
+        }
+
+    }
+}
 
