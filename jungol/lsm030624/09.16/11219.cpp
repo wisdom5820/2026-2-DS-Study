528c49c9f84c65ffb5276008a3916c1b0cd0bed5
lsm030624 <126699042+lsm030624@users.noreply.github.com>
2026-09-18 23:13:35 +0900

    Update 11219.cpp

--- jungol/lsm030624/09.16/11219.cpp
+++ jungol/lsm030624/09.16/11219.cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<int, pair<char, int>> m;
+
+    for (int x = 0; x < n; x++) {
+        int i, k;
+        char j;
+
+        cin >> i >> j >> k;
+
+        m[i] = {j, k};
+    }
+
+    int N;
+    cin >> N;
+
+    for (int x = 0; x < n; x++) {
+        auto [j, k] = m[N];
+
+        if (j == 'L'){
+            N -= k;
+        }
+        else{
+            N += k;
+        }
+    }
+    cout << N;
+}
 
