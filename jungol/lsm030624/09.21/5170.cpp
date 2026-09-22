4f702d400dd49c6f97d690268f98d1e2deb1aa64
lsm030624 <126699042+lsm030624@users.noreply.github.com>
2026-09-23 08:54:27 +0900

    Update 5170.cpp

--- jungol/lsm030624/09.21/5170.cpp
+++ jungol/lsm030624/09.21/5170.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main() {
-    // 입출력 속도 향상
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -29,19 +28,19 @@ int main() {
         long long mid = (low + high) / 2;
         long long sum = 0;
 
-        // mid 높이로 잘랐을 때 얻을 수 있는 나무 총길이 계산
+
         for (int i = 0; i < N; i++) {
             if (trees[i] > mid) {
                 sum += (trees[i] - mid);
             }
         }
 
-        // 가져갈 수 있는 나무의 길이가 M 이상이면 높이를 더 높여본다.
+
         if (sum >= M) {
-            ans = mid;      // 조건 만족하므로 일단 정답 후보 저장
-            low = mid + 1;  // 더 높은 H 탐색
+            ans = mid;     
+            low = mid + 1; 
         } else {
-            high = mid - 1; // M 미만이면 절단기 높이를 낮춰야 함
+            high = mid - 1;
         }
     }
 
